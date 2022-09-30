0e2d04aa02f035ffc65ecc8d5bf2dcc59746ee07
Kiet Nguyen <6393445-19520666NK@users.noreply.gitlab.com>
2022-09-30 21:35:15 +0700

    change endpoint

--- src/main/java/com/dailycodebuffer/springboottutorial/controller/Controller.java
+++ src/main/java/com/dailycodebuffer/springboottutorial/controller/Controller.java
@@ -8,6 +8,6 @@ public class Controller {
 
     @RequestMapping(path = "/")
     public String helloSpring() {
-        return "Hello chao mung anh em den voi channel cua minh!";
+        return "Hello chao mung anh em den voi channel xinh dep cua minh!";
     }
 }
